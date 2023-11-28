3769bb7c06d6a34dd01b3fbf1c97296a8d7fd88b
mengjh <mengjh@zdiai.com>
2023-11-28 13:51:39 +0800

    初始化sql上传

---
+++ sql/init/t_ship_info.sql
@@ -0,0 +1,7 @@
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (1, 'NewRenown', '海上真理', '0');
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (2, 'MaritimeVerity', '海上真理', '0');
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (3, 'MaritimeValor', '海上勇士', '0');
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (5, 'NewProsperity', '凯荣', '0');
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (6, 'NewCourage', '凯昇', '0');
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (7, 'MaritimeGuardian', '海上干嘛', '0');
+INSERT INTO `t_ship_info` (`id`, `ship_code`, `ship_name_zh`, `is_enable`) VALUES (8, 'CSSC_GLADSTONE', NULL, '0');
